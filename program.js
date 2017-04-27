12d4f2e8503adcfb2af0609c3d803fe50c9f25d7
Kenny Lu <klu@realmatters.com>
2017-04-27 15:49:56 -0400

    lesson 9

--- program.js
+++ program.js
@@ -1,11 +1,14 @@
-var koa = require('koa');
-var app = new koa();
+const koa = require('koa');
+const session = require('koa-session');
 
+const app = new koa();
 app.keys = ['secret', 'keys'];
 
+app.use(session(app));
+
 app.use(function* () {
-  var times = ~~this.cookies.get('view', { signed: true }) + 1;
-  this.cookies.set('view', times, { signed: true });
+  const times = ~~this.session.views + 1;
+  this.session.views = times;
   this.body = `${times} views`;
 });
 
