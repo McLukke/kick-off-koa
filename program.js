8f58748453410fb91d920f3320e3885de4f5492d
Kenny Lu <klu@realmatters.com>
2017-04-27 14:34:14 -0400

    lesson 5

--- program.js
+++ program.js
@@ -1,18 +1,13 @@
 var koa = require('koa');
 var app = new koa();
-var parse = require('co-body');
-var fs = require('fs');
 
-app.use(function *(next) {
-  if (this.path !== '/json') return yield next;
-
-  this.body = { foo: 'bar' };
-});
-
-app.use(function *(next) {
-  if (this.path !== '/stream') return yield next;
-
-  this.body = fs.createReadStream(process.argv[3]);
+app.use(function *(next){
+  console.log(this.request.is());
+  if (this.request.is() === 'application/json') {
+    this.body = { message: 'hi!' };
+  } else {
+    this.body = 'ok';
+  }
 });
 
 app.listen(process.argv[2]);
