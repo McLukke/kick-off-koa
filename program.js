6a6d502fe41df1f77185dc3ab08e4cdb7256e37b
Kenny Lu <klu@realmatters.com>
2017-04-27 13:47:26 -0400

    lesson 2 vsn 2

--- program.js
+++ program.js
@@ -2,15 +2,21 @@ var koa = require('koa');
 var app = new koa();
 
 app.use(function *(next) {
-  if (this.path === '/') {
-    this.body = 'hello koa';
-  } else if (this.path === '/404') {
-    this.body = 'page not found';
-  } else if (this.path === '/500') {
-    this.body = 'internal server error';
-  } else {
-    return yield next;
-  }
+  if (this.path !== '/') return yield next;
+
+  this.body = 'hello koa';
+});
+
+app.use(function *(next) {
+  if (this.path !== '/404') return yield next;
+
+  this.body = 'page not found';
+});
+
+app.use(function *(next) {
+  if (this.path !== '/500') return yield next;
+
+  this.body = 'internal server error';
 });
 
 app.listen(process.argv[2]);
